05d33cf10ca2867a10e44c68f0902dee2c3352a3
Najeeb <naxibkhan@gmail.com>
2020-10-29 00:29:03 +0500

    material ui card component

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid, Toolbar, Typography, AppBar } from "@material-ui/core";
+import AcUnitIcon from "@material-ui/icons/AcUnit";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography className={classes.typographyStyles}>
+          This is our Header
+        </Typography>
+        <AcUnitIcon />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
